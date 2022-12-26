a9f63c1f014f589e6e3598603ea8668a93243711
JasonShuyinta <jasonshuyinta@gmail.com>
2022-12-26 22:10:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Avatar, Container, Divider } from "@mui/material";
+import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        height: "100%",
+      }}
+    >
+      <Navbar />
+      <div className="glass">
+        <div className="center-align">
+          <Avatar
+            src="/img/Avatar.jpeg"
+            alt="profile"
+            style={{ width: "80px", height: "80px" }}
+          />
+          <div style={{textAlign:"center"}}>
+            <h1>Hi! I'm Jason</h1>
+            <div >
+            <code>BACKEND/CLOUD DEVELOPER <img src="assets/data-server.png" alt="server" style={{width: "1rem", height: "1rem"}} /> ☁️ </code> 
+            </div>
+          </div>
+            <Divider style={{height: "10px", width: "90%", color: "black", margin: "1rem 0"}}/>
+          <div className="text-container">
+            <p>
+              My name is Jason and I'm a Web Developer with 3+ years of
+              experience.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
